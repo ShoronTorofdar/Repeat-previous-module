74ae372f024d9f7f7154ba7ea433b6f49f6399ff
ShoronTorofdar <86644344+ShoronTorofdar@users.noreply.github.com>
2022-02-11 08:52:31 +0600

    more example of API second time

---
+++ explore-meal-db/js/mealDB.js
@@ -0,0 +1,20 @@
+const searchFood = () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  searchField.value = "";
+
+   const url = `
+   https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}
+   `;
+
+   fetch(url)
+    .then(response => response.json())
+    .then(data => displaySearchResult(data.meals));   
+}
+
+const displaySearchResult = meals =>{
+  meals.map(meal => {
+
+    
+  });
+}
