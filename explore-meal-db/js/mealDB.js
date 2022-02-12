8d531a44d028d612ee04cb9dba5a02cc6931c552
ShoronTorofdar <86644344+ShoronTorofdar@users.noreply.github.com>
2022-02-12 10:00:25 +0600

    33-7

--- explore-meal-db/js/mealDB.js
+++ explore-meal-db/js/mealDB.js
@@ -12,9 +12,40 @@ const searchFood = () => {
     .then(data => displaySearchResult(data.meals));   
 }
 
-const displaySearchResult = meals =>{
-  meals.map(meal => {
+// display section start
 
-    
+const displaySearchResult = meals =>{
+  const displayCard = document.getElementById("display-card");
+  displayCard.textContent = "";
+
+
+  meals.map((meal) => {
+    const card = document.createElement('div');
+    card.classList.add('col')
+    card.innerHTML = `
+    <div onclick = "loadMealDetail()"; class="card">
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title fs-2">${meal.strMeal}</h5>
+      <p class="card-text">${meal.strInstructions.slice(0,200)}</p>
+    </div>
+    <div class="card-footer d-flex justify-content-between">
+      <a href="${meal.strSource}" target="_blank" class="btn btn-primary">Source</a>
+      <a href="${meal.strYoutube}" target="_blank" class="btn btn-primary">Youtube</a>
+    </div>
+  </div>
+    `
+    displayCard.appendChild(card)
   });
+}
+
+const loadMealDetail = (mealId) => {
+const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`
+fetch(url)
+  .then(response => response.json())
+  .then(data => displayMealDetail(data.meals[0]));
+}
+
+const displayMealDetail = meal => {
+console.log(displayMealDetail);
 }
