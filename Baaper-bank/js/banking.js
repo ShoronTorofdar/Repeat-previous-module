e8f0f47198a981425dcb3e0b974f3f4eb0e43825
ShoronTorofdar <86644344+ShoronTorofdar@users.noreply.github.com>
2022-02-13 17:06:46 +0600

    functional bank start

--- Baaper-bank/js/banking.js
+++ Baaper-bank/js/banking.js
@@ -1,1 +1,64 @@
-console.log('Allah is great')
+
+function getInputValue(){
+  const depositInput= document.getElementById('deposit-input');
+const depositAmountText = depositInput.value;
+const depositAmount = parseFloat(depositAmountText);
+}
+
+
+
+
+
+document.getElementById('deposit-button').addEventListener('click', ()=>{
+const depositInput= document.getElementById('deposit-input');
+const depositAmountText = depositInput.value;
+const depositAmount = parseFloat(depositAmountText);
+
+
+// get current deposit
+const depositTotal = document.getElementById('deposit-total')
+const depositTotalText = depositTotal.innerText;
+const previousDepositTotal = parseFloat(depositTotalText);
+depositTotal.innerText = previousDepositTotal + depositAmount;
+console.log(previousDepositTotal);
+
+// update balance
+const balanceTotal = document.getElementById('balance-total');
+const balanceTotalText = balanceTotal.innerText;
+const previousBalanceTotal = parseFloat(balanceTotalText);
+balanceTotal.innerText = previousBalanceTotal + depositAmount;
+
+
+// clear input field
+depositInput.value = '';
+   
+});
+
+// handle withdraw button
+document.getElementById('withdraw-button').addEventListener('click',()=>{
+  const withdrawInput = document.getElementById('withdraw-input');
+  const withdrawAmountText = withdrawInput.value;
+  const withdrawAmount = parseFloat(withdrawAmountText)
+  console.log(withdrawAmountText);
+
+  // update withdraw toal
+const withdrawTotal = document.getElementById('withdraw-total');
+const previousWithdrawTotalText = withdrawTotal.innerText;
+const previousWithdrawTotal = parseFloat(previousWithdrawTotalText)
+withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount;
+
+// update balance total after withdraw
+const balanceTotal = document.getElementById('balance-total');
+const balanceTotalText = balanceTotal.innerText;
+const previousBalanceTotal = parseFloat(balanceTotalText);
+balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
+// clear withdraw input field
+withdrawInput.value = ''
+
+})
+
+
+
+
+
+
